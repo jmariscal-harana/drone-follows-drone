bc54ee9f7bfa5474a467d3412da1570482c75bee
jmh <jorge.mariscal.harana@gmail.com>
2024-11-23 21:00:16 +0000

    feat: add takeoff and landing to control

--- src/control/drone_control.py
+++ src/control/drone_control.py
@@ -1,12 +1,23 @@
+from src.sdk.drone_sdk import DroneSDK
+from time import sleep
 class DroneControl:
-    def __init__(self) -> None:
+    def __init__(self, sdk: DroneSDK) -> None:
         """Initialise the drone control algorithm."""
-        print("Control init!")
+        print("Control init...")
+
+        self.sdk = sdk
 
     def close(self) -> None:
         """Close the drone control algorithm."""
         print("Closing drone control!")
 
-    def control(self, state, inputs) -> None:
+        self.sdk.send_command("land")
+
+    def control(self, inputs) -> None:
         """Send commands to the drone."""
         print("Controlling drone!")
+
+        # TODO: Implement drone control logic here
+        command = "takeoff"
+        
+        self.sdk.send_command(command)
